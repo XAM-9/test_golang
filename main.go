cd864f428d103c2632de389fdf2a1c0554c242c8
xam-9 <xamtrap99m@gmail.com>
2025-03-17 18:19:07 +0700

    if else

--- main.go
+++ main.go
@@ -1,9 +1,33 @@
 package main
 
-import "fmt"
-
 func main() {
-	fullname := "Nakarin Wongsang"               // variable declaration
-	age := 20                                    // variable declaration
-	fmt.Printf("Hello %s age %d", fullname, age) // print to console
+	// fullname := "Nakarin Wongsang"                  // variable declaration
+	// age := 20                                       // variable declaration
+	// fmt.Printf("Hello %s age %d \n", fullname, age) // print to console
+
+	// age = age + 2                                   // increment age by 1
+	// fmt.Printf("age is now %d", age)                // print to console
+
+	//-----------------------------------------------------------------
+	// var score = 62
+
+	// if score >= 70 {
+	// 	fmt.Println("Pass")
+	// } else {
+	// 	fmt.Println("Fail")
+	// }
+
+	//-----------------------------------------------------------------
+	// var week = 5
+	// switch week {
+	// case 1:
+	// 	fmt.Println("Monday")
+	// case 2:
+	// 	fmt.Println("Tuesday")
+	// default:
+	// 	fmt.Println("Invalid")
+	// }
 }
+
+// Hello Nakarin age 20
+// age is now 21
